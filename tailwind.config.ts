0961262821bdf6fcdacc52997153dadd28adc3f2
kadeem254 <kadeemalexander254@gmail.com>
2024-09-12 09:35:43 +0300

    INIT

---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+import { Config } from "tailwindcss";
+
+/** @type {import('tailwindcss').Config} */
+const config: Config = {
+	content: ["./index.html"],
+	theme: {
+		extend: {
+			container: {
+				center: true,
+				padding: "20px",
+			},
+			colors: {
+				primary: {
+					base: "#2D6A4F",
+					light: "#3D8F69",
+					dark: "#255640",
+				},
+				secondary: {
+					base: "#F4F1DE",
+					light: "#FAF9F0",
+					dark: "#F0ECD1",
+				},
+				accent: {
+					base: "#8D8741",
+					light: "#EDE9C1",
+					medium: "#A8A04D",
+					dark: "#706B33",
+				},
+			},
+			fontFamily: {
+				montserrat: ["Montserrat", "sans-serif"],
+				"playfair-display": ["Playfair Display", "serif"],
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
